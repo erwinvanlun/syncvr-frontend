9fe93a8bb1e52dd1229314c8a2f5af2873d94ee4
Erwin van Lun <erwin@vanlun.com>
2021-09-24 20:24:06 +0200

    now gradually loads older history as well automatically

--- src/app/fibonacci/fibonacci.service.ts
+++ src/app/fibonacci/fibonacci.service.ts
@@ -22,6 +22,7 @@ export interface ServiceFibonacciHistory extends APIFibonacciNumberMeta {
 })
 export class FibonacciService {
 
+  history: ServiceFibonacciHistory[] = [];
   head = 0;
   tail = 0;
 
@@ -51,31 +52,47 @@ export class FibonacciService {
 
   public loadHistory$(): Observable<ServiceFibonacciHistory[]> {
     const url = environment.fibonacciApi + '/' + APIFibonacci.history;
-    const request: APIFibonacciHistoryRequest = {head: this.head, tail: this.tail, maxTailingRows: environment.maxRows}
+    console.log('now loading with head ' + this.head + ' tail: ' + this.tail + 'en rows: ' + environment.maxRows);
+    let request: APIFibonacciHistoryRequest = {head: this.head, tail: this.tail, maxTailingRows: environment.maxRows}
     return merge(this._forceHistory, timer(1, environment.historyRefreshInSeconds * 1000)).pipe(
       // todo actually historyRefreshInSeconds is more config than environment
+      map(() => {
+        // is map the best way to make sure head and tail are updated in next loops?
+        request = {head: this.head, tail: this.tail, maxTailingRows: environment.maxRows}
+      }),
       switchMap(() =>
         this.httpService.post$<APIFibonacciHistoryResponse>(url, request, httpOptions) // todo: we actually need to check whether previous get is finished
           .pipe(
             map((response: APIFibonacciHistoryResponse) => {
-              let serviceHistory: ServiceFibonacciHistory[];
-              serviceHistory = response.history.map((hr) => ({
+              let receivedHistory: ServiceFibonacciHistory[];
+              console.log('comparing to');
+              console.log(this.head);
+              console.log('receivedHistory lenghth');
+              console.log(response.history.length);
+              receivedHistory = response.history.map((hr) => ({
                 ...hr,
                 myRequest: this.myRequests.includes(hr.requestId),
                 new: hr.requestId > this.head
               }));
 
-              this.head = (serviceHistory.map((r) => r.requestId))
-                .reduce((r1, r2) => Math.max(r1, r2));
+              // merge received with current, filter for duplicates
+              let ids = new Set(this.history.map(r => r.requestId));
+              this.history = [...this.history, ...receivedHistory.filter(r => !ids.has(r.requestId))].
+              sort((r1, r2) => r2.requestId - r1.requestId);
 
-              this.tail = (serviceHistory.map((r) => r.requestId))
+              this.head = (this.history.map((r) => r.requestId))
+                .reduce((r1, r2) => Math.max(r1, r2));
+              console.log('setted head to: ');
+              console.log(this.head);
+
+              this.tail = (this.history.map((r) => r.requestId))
                 .reduce((r1, r2) => Math.min(r1, r2));
               console.log('setted tail to: ');
               console.log(this.tail);
 
-              return serviceHistory;
+              return this.history;
             }))),
-      retry(), // retry on failure
+      //retry(), // retry on failure
       share(), // share this between all subscribers
       takeUntil(this._polling)  // until service is stopped
     );
